6a30bb889ea8f0fb0a2f7b524514037a9e8b5b95
admin44449999 <108731844+admin44449999@users.noreply.github.com>
2024-07-02 12:31:43 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -1,16 +1,16 @@
 class Config(object):
-    LOGGER = True
-    API_ID =None 
-    API_HASH = ""
-    TOKEN = ""  
-    OWNER_ID=None
+    LOGGER =True
+    API_ID =23080322 
+    API_HASH = "b3611c291bf82d917637d61e4a136535"
+    TOKEN = "7271288704:AAF3f9OJQ_8GEZL7y3NEoYbjc7TslSeq6lY"  
+    OWNER_ID=7011929837
     
-    SUPPORT_CHAT = "" 
+    SUPPORT_CHAT = "-1002150515664" 
     START_IMG = ""
     EVENT_LOGS = ()
     MONGO_DB_URI= ""
    
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgresql://postgres.adldmnbzdckqwrckabtj:Aaryanary1097@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""
     )
